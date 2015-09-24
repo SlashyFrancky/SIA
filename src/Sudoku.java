1dc8e4dc92859109b8cea66dca56717bdc876860
SlashyFrancky <asterix204@hotmail.com>
2015-09-24 19:21:03 -0400

    Allo

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,7 +3,7 @@ public class Sudoku {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int a;
 		String test = "Hello World";
 		System.out.println(test);
 	}
