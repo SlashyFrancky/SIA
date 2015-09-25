bd08ab159d325de44ab145268058d6455e90aef3
SlashyFrancky <asterix204@hotmail.com>
2015-09-25 12:09:57 -0400

    isComplete dans Rules. Régler le bug

--- src/Rules.java
+++ src/Rules.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Rules {
 
+	//Définit si un choix est légal 
 	public boolean isLegal(int[][]grid, int choice, int i, int j){
 		int [][] tempGrid = new int[grid.length][];
 		
@@ -32,6 +34,7 @@ public class Rules {
 		return true;
 	}
 	
+	// Renvoie la liste des choix légaux
 	public ArrayList<Integer> legalChoices(int[][]grid, int i, int j){
 		ArrayList<Integer> legalChoices = new ArrayList<Integer>();
 		
@@ -39,5 +42,59 @@ public class Rules {
 			if(isLegal(grid, c,  i, j)) legalChoices.add(c);
 		}
 		return legalChoices;
+	}
+	
+	// Vérifie si la grille est complète 
+	public boolean isComplete(int[][] grid){
+		boolean[] numbers = new boolean[grid.length];
+		int content; 
+		
+		for (int i=0; i<grid.length;i++){
+			Arrays.fill(numbers, false);
+			for (int j=0; j<grid.length; j++){
+				content = grid[i][j];
+				if (content == 0)return false; 
+				if(numbers[content-1] == false){
+					numbers[content-1] = true; 
+				}else{
+					return false;
+				}
+			}
+		}
+		
+		for (int j=0; j<grid.length;j++){
+			Arrays.fill(numbers, false);
+			for (int i=0; i<grid.length; i++){
+				content = grid[i][j];
+				if (content == 0) return false;
+				if(numbers[content-1] == false){
+					numbers[content-1] = true; 
+				}else{
+					return false; 
+				}
+			}
+		}
+		
+		//BUG 
+		for(int i = 0; i<3; i++){
+			for(int j = 0; j < 3; j++){
+				Arrays.fill(numbers, false);
+				for(int bi = 0; bi < 3; i++){
+					for(int bj = 0; bj <3; bj++){
+						content = grid[3*i + bi][3*j+bj]; 
+						if (content == 0) return false;
+						if (numbers[content-1] == false){
+							numbers[content-1] = true;
+						}
+						else{
+							System.out.println(content + " " +(3*i + bi) + (3*j + bj));
+							return false; 
+						}
+					}
+				}
+			}
+		}
+		return true; 
+		
 	}
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -10,23 +10,37 @@ import java.util.ArrayList;
 public class Sudoku {
 	
 	private static int SIZE = 9;  
-	int[][] grid; 
+	int[][] grid;
+	boolean[][] playable;
 	
 	public Sudoku(String s){
 		this.grid = createGrid(s);
+		this.playable = createPlayable(); 
 	}
 	
 	public int[][] createGrid(String s){
 		int[][] grid = new int[SIZE][SIZE];
-		int n=0; 
+		int n=0;
+		int content;
 		
 		for(int i=0; i<SIZE; i++){
 			for(int j=0; j<SIZE; j++){
-				grid[i][j] = Character.getNumericValue(s.charAt(n));
+				content = Character.getNumericValue(s.charAt(n));
+				grid[i][j] = content; 
 				n++; 
 			}
 		}
 		return grid;
+	}
+	
+	public boolean[][] createPlayable(){
+		boolean[][] playable = new boolean[SIZE][SIZE];
+		for(int i=0; i<SIZE; i++){
+			for(int j=0; j<SIZE; j++){
+				if (this.grid[i][j] > 0) playable[i][j] = false;
+			}
+		}
+		return playable;
 	}
 	
 	public String toString(){
@@ -45,14 +59,14 @@ public class Sudoku {
 
 	public static void main(String[] args) throws IOException {
 		
-		TextFileLineParser lp = new TextFileLineParser("1sudoku.txt");
+		TextFileLineParser lp = new TextFileLineParser("completeSudoku.txt");
 		ArrayList<String> sudokus = lp.getLines();
 		Sudoku s = new Sudoku(sudokus.get(0));
 		
 		Rules rules = new Rules(); 
 		
 		System.out.println(rules.legalChoices(s.grid, 8, 0));
-		
+		System.out.println(rules.isComplete(s.grid));
 		
 		System.out.println(s);
 		
