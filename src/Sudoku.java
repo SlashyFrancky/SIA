cc0f9f570eead4da4e991560745a28bd647b69f7
SlashyFrancky <asterix204@hotmail.com>
2015-09-24 20:27:20 -0400

    .

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -6,13 +6,12 @@ public class Sudoku {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a;
 		String test = "Hello World";
 		System.out.println(test);
 		
-		BreadthFirstSearch BFSearch = new BreadthFirstSearch(null);
-		HillClimbingSearch HCsearch = new HillClimbingSearch(null);
-		AStarSearch ASSearch = new AStarSearch(null, null);
+		BreadthFirstSearch BFSearch = new BreadthFirstSearch();
+		//HillClimbingSearch HCsearch = new HillClimbingSearch(null);
+		//AStarSearch ASSearch = new AStarSearch(null, null);
 	}
 
 }
